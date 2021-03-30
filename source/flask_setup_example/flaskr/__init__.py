0967a51569f34d1c7331a55f852d036c68cc5013
Robin Smorenburg <robin@smorenburg.io>
2021-03-30 08:02:19 +0200

    Add basic flask app

---
+++ source/flask_setup_example/flaskr/__init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import os
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def hello():
+        return jsonify({
+            'message': 'Hello World!'
+        })
+
+    @app.route('/smiley')
+    def smiley():
+        return ':)'
+
+    return app
