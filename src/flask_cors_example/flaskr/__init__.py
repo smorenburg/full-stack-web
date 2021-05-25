b91a14b4ddff511dbf2503bd4048579ff925f1aa
Robin Smorenburg <robin@smorenburg.io>
2021-05-25 09:13:24 +0200

    Modify folder name

---
+++ src/flask_cors_example/flaskr/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            'Access-Control-Allow-Headers',
+            'Content-Type, Authorization'
+        )
+        response.headers.add(
+            'Access-Control-Allow-Methods',
+            'GET, POST, PATCH, DELETE, OPTIONS'
+        )
+
+        return response
+
+    @app.route('/')
+    def hello():
+        return jsonify({'message': 'Hello World!'})
+
+    return app
